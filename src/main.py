29fb3dc5de66190d31ae474573f350ce478595be
Andre Gilbert <andre.gilbert.77110@gmail.com>
2023-12-13 16:03:34 +0100

    added multiple scenes example

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
 """Manim entrypoint."""
-from manim import *
+from manim import PINK, YELLOW, Circle, Create, Scene
 
 
 class CreateCircle(Scene):
-    """Example manim class."""
-
     def construct(self):
         circle = Circle()  # create a circle
         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen
+
+
+class CreateCircle2(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(YELLOW, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
+
+class PCA(Scene):
+    """Class that implements a manim scene for visualizing principal component analysis."""
+
+    def construct(self) -> None:
+        scenes = [CreateCircle, CreateCircle2]
+        for scene in scenes:
+            scene.construct(self)
