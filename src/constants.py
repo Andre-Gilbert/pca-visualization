76361e0cb7365f5ca863d25c44c4e49764eca6ad
jhb300 <janhenrik.bertrand@gmail.com>
2023-12-20 18:28:22 +0100

    Added font sizes and fixed centered matrix formula

--- src/constants.py
+++ src/constants.py
@@ -3,10 +3,17 @@ from manim import *
 
 
 class Formulas:
-    CENTERED_MATRIX = MathTex(r"\(\textbf{B} = X - \bar{X}\)")
+    CENTERED_MATRIX = MathTex(r"\textbf{B} = X - \bar{X}")
     COVARIANCE = MathTex(
         r"\operatorname{cov}[X_i, X_j] = \operatorname{E}[(X_i - \operatorname{E}[X_i])(X_j - \operatorname{E}[X_j])]"
     )
     COVARIANCE_SIMPLIFIED = MathTex(r"\operatorname{cov}[X_i, X_j] = \operatorname{E}[X_i \cdot X_j]")
     COVARIANCE_MATRIX = MathTex(r"Cov(X) = \frac{1}{i}(B^TB)")
     PRINCIPAL_COMPONENTS = MathTex(r"T = E_b \cdot B")
+
+class FontSize:
+    HEADING1 = 38
+    HEADING2 = 34
+    HEADING3 = 30
+    HEADING4 = 26
+    TEXT = 24
