0848ccd59dc2cc6300d62f793bf2929ebb2385e6
jhb300 <janhenrik.bertrand@gmail.com>
2023-12-21 22:58:17 +0100

    Generated, added and build in sample dataset.

--- src/pca.py
+++ src/pca.py
@@ -1,15 +1,28 @@
 """PCA scene."""
 from manim import *
+import pandas as pd
 
 
 class PCAExplained(ThreeDScene):
     def construct(self):
+        # Create axes
         axes = ThreeDAxes()
-        circle = Circle()
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
-        self.add(circle, axes)
-        self.begin_ambient_camera_rotation(rate=0.1)
-        self.wait()
+        self.begin_ambient_camera_rotation(rate=0.2)
+        
+        # Read data from CSV
+        data = pd.read_csv("assets/data_points.csv")
+        points_data = data.values  # Assuming columns are x, y, z
+        
+        # Create scatter plot using Points class
+        scatter_points = VGroup(
+            *[Dot3D(point=np.array(point), color=BLUE, radius=0.05) for point in points_data]
+        )
+        
+        # Add scatter plot to the scene
+        self.play(Create(axes), Create(scatter_points))
+        self.wait(4)
+        
         self.stop_ambient_camera_rotation()
         self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES)
         self.wait()
